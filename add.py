eba5087ca85218f56ab973eb7c1f2fe575d6910c
haleytolar <153100456+haleytolar@users.noreply.github.com>
2024-11-06 02:09:06 +0000

    added installs screenshots

--- add.py
+++
@@ -1,9 +0,0 @@
-# Get input from the user and convert it to an integer
-a = int(input("Enter the first number: "))
-b = int(input("Enter the second number: "))
-
-# Add the two numbers
-c = a + b
-
-# Print the result
-print("The sum of", a, "and", b, "is", c)
